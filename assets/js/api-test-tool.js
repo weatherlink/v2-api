2ddb22868f853b92a4cb6bb67834c65866784c18
Jeremy Wall <jeremyw@davisnet.com>
2019-07-03 16:51:08 -0700

    Update api-test-tool.js

--- assets/js/api-test-tool.js
+++ assets/js/api-test-tool.js
@@ -1,1 +1,15 @@
+if ($('input#api-timestamp').length > 0) {
+  setInterval(updateAPITimestamp, 1000);
+}
 
+function updateAPITimestamp() {
+	$('input#api-timestamp').val(Math.floor(Date.now() / 1000));
+}
+
+$('select#api-endpoint').change(function() {
+  $('div.params').hide();
+  var selectedVal = $('select#api-endpoint').find(':selected').text();
+  if (selectedVal != '') {
+  	 $('div#params-' + selectedVal).show();
+  }
+});
