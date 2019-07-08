8ff0aa3bab380bae18533720a5985ff250becdeb
Jeremy Wall <jeremyw@davisnet.com>
2019-07-08 12:13:33 -0700

    Update api-test-tool.js

--- assets/js/api-test-tool.js
+++ assets/js/api-test-tool.js
@@ -15,34 +15,70 @@ $('select#api-endpoint').change(function() {
 });
 
 function calculateAPISignature() {
-	/*
-	var _ = require("lodash");
-	var apiKey = pm.environment.get("V2_API_KEY");
-	var apiSecret = pm.environment.get("V2_API_SECRET");
-	var t = Math.round((new Date()).getTime() / 1000);
-	pm.environment.set("V2_API_TIMESTAMP", t);
-	var requestUrlObject = JSON.parse(JSON.stringify(pm.request.url));
-	var stringToHash = _.chain(requestUrlObject.query)
-    		.concat(requestUrlObject.variable)
-    		.filter(function(param) {
-        		if (param.key == "api-key" || param.key == "api-signature" || param.key == "t" || _.get(param, "disabled", false) === true) {
-            			return false;
-        		}
-        		return true;
-    		})
-    		.concat([
-        		{"key": "api-key", "value": apiKey},
-        		{"key": "t",       "value": t}
-    		])
+	var apiEndpoint = $('select#api-endpoint').find(':selected').val();
+	
+	if (apiEndpoint == '') {
+		alert("Please select an API Endpoint");
+		return;
+	}
+	
+	var apiKey = $('input#api-key').val();
+	var apiSecret = $('input#api-secret').val();
+	var t = $('input#api-timestamp').val();
+	
+	var queryParams = [
+		{"key": "api-key", "value": apiKey},
+		{"key": "t",       "value": t}
+	];
+	var pathParams = [];
+	
+	switch (apiEndpoint) {
+		case 'stations-all':
+			break;
+		case 'stations-some':
+			pathParams.push({"key": "station-ids", "value": $('input#stations-some-station-ids').val()});
+			break;
+		case 'nodes-all':
+			break;
+		case 'nodes-some':
+			pathParams.push({"key": "node-ids", "value": $('input#nodes-some-node-ids').val()});
+			break;
+		case 'sensors-all':
+			break;
+		case 'sensors-some':
+			pathParams.push({"key": "sensor-ids", "value": $('input#sensors-some-sensor-ids').val()});
+			break;
+		case 'sensor-activity-all':
+			break;
+		case 'sensor-activity-some':
+			pathParams.push({"key": "sensor-ids", "value": $('input#sensor-activity-some-sensor-ids').val()});
+			break;
+		case 'sensor-catalog':
+			break;
+		case 'current':
+			pathParams.push({"key": "station-id", "value": $('input#current-station-id').val()});
+			break;
+		case 'historic':
+			pathParams.push({"key": "station-id", "value": $('input#historic-station-id').val()});
+			break;
+	}
+
+	var stringToHash = _.chain(queryParams)
+		.concat(pathParams)
     		.orderBy("key")
     		.map(function(param) {
         		return param.key + param.value;
     		})
     		.join("")
     		.value();
-	var apiSignature = CryptoJS.HmacSHA256(stringToHash, apiSecret).toString(CryptoJS.enc.Hex);
-	pm.environment.set("V2_API_SIGNATURE", apiSignature);
-	*/
+
+	var hmac = forge.hmac.create();
+	hmac.start('sha256', apiSecret);
+	hmac.update(stringToHash);
+	var apiSignature = hmac.digest().toHex();
+	
+	console.log(stringToHash);
+	console.log(apiSignature);
 }
 
 $('input#send-api-request-button').on('click', function(eventData) {
