d2533f0e0138499c02ca43c697dba1079d0f639d
Jeremy Wall <jeremyw@davisnet.com>
2019-07-10 11:16:48 -0700

    Update api-test-tool.js

--- assets/js/api-test-tool.js
+++ assets/js/api-test-tool.js
@@ -113,6 +113,7 @@ function sendAPIRequest() {
 
 	var jqxhr = $.getJSON('https://enviro-testapi.weatherlink.com/v2' + apiEndpointPath, requestData)
 		.done(function(responseJson) {
+			editor.moveCursorTo(0, 0);
 			editor.setValue(JSON.stringify(responseJson, null, 4), -1);
 		})
 		.fail(function(jqxhr, textStatus, error) {
