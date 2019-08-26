7ef32a3789ea5f9f5aff1f24b927997bbe015b73
Jeremy Wall <jeremyw@davisinstruments.com>
2019-08-26 16:22:24 -0700

    Delete footer-copyright.js

--- assets/js/footer-copyright.js
+++
@@ -1,3 +0,0 @@
-$('div.page__footer-follow ul.social-icons > li').has('a[href="/v2-api/feed.xml"]').remove();
-
-$('div.page__footer-copyright').html('Copyright &copy; ' + new Date().getFullYear() + ' <a href="https://www.davisinstruments.com">Davis Instruments</a> - All Rights Reserved');
