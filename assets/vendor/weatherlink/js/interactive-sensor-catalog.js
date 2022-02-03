acda52a31e006d5800f10ad006b9a12e3da67801
Jeremy Wall <jeremyw@davisinstruments.com>
2022-02-02 17:08:12 -0800

    wip

--- assets/vendor/weatherlink/js/interactive-sensor-catalog.js
+++ assets/vendor/weatherlink/js/interactive-sensor-catalog.js
@@ -6,6 +6,25 @@ $('select#sensor').change(function() {
 //	}
 });
 
+var SENSOR_CATALOG_JSON = null;
+
+var jqxhr = $.getJSON("/v2-api/resources/universal-sensor-catalog.json")
+	.done(function(json) {
+		SENSOR_CATALOG_JSON = json;
+		populateSensorSelect();
+	});
+
+function populateSensorSelect() {
+	if (SENSOR_CATALOG_JSON == null) {
+		return;
+	}
+	$('select#sensor').empty();
+	for (var sensorTypeIndex = 0 ; sensorTypeIndex < SENSOR_CATALOG_JSON.sensor_types.length ; sensorTypeIndex++) {
+		var sensorType = SENSOR_CATALOG_JSON.sensor_types[sensorTypeIndex];
+		$('select#sensor').append($("<option value='"+sensorType.sensor_type+"'>Sensor Type "+sensorType.sensor_type+" - "+sensorType.product_name+"</option>"));
+	}
+}
+
 function displayCatalogDetails() {
 	var catalogDiv = $('div#catalog');
 	catalogDiv.html('');
